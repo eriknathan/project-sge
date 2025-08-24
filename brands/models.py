0c2097fe3c51b0956a22f59909260c67047a7d00
Erik Nathan <77215294+eriknathan@users.noreply.github.com>
2025-08-24 16:10:48 -0300

    adding brand app

---
+++ brands/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+
+    def __str__(self):
+        return self.name
